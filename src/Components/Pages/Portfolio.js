6a02b48be29fcc4c0f44857dab70a28fdb7bc9d8
Zharyk <jaryk8881@gmail.com>
2022-07-02 12:53:28 +0600

    build project page

--- src/Components/Pages/Portfolio.js
+++ src/Components/Pages/Portfolio.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { projects } from '../../Data/Datas'
-import { Link } from "react-router-dom"
+import { BsEmojiSmileUpsideDown } from "react-icons/bs"
 import '../../Styles/Projects/Projects.css'
 
 
@@ -11,7 +10,10 @@ function Portfolio() {
   return (
     <motion.div className="main-container">
       <motion.div className="title">My projects</motion.div>
-      <motion.div className="items">{projects.map(items => <a href={items.path} target="_blank"><motion.div className="images"><img src="https://i.pinimg.com/1200x/8b/da/ca/8bdaca81d5ddbaeb92b61d6b5787d866.jpg" /><p>{items.name}</p></motion.div></a>)}</motion.div>
+      <motion.div className="content-work">
+        Comming soon!
+        <BsEmojiSmileUpsideDown className="icon" />
+      </motion.div>
     </motion.div>
   );
 }
